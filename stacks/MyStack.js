a041b0ce1e0e279573ce1284d1e15c91ff83888f
blakegreendev <bgreen@intervision.com>
2021-10-26 09:34:39 -0400

    initial commit

---
+++ stacks/MyStack.js
@@ -0,0 +1,28 @@
+import * as sst from "@serverless-stack/resources";
+
+export default class MyStack extends sst.Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const table = new sst.Table(this, "Counter", {
+      fields: {
+        counter: sst.TableFieldType.STRING
+      },
+      primaryIndex: { partitionKey: "counter" },
+    });
+
+    const site = new sst.NextjsSite(this, "Site", {
+      path: "frontend",
+      environment: {
+        REGION: scope.region,
+        TABLE_NAME: table.tableName
+      }
+    });
+
+    site.attachPermissions([table]);
+
+    this.addOutputs({
+      URL: site.url,
+    });
+  }
+}
